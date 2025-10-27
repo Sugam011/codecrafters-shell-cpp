9c48bc31a07a62f2f3ce67f09bc975a9ad4d67ea
Sugam011 <sugam0519@gmail.com>
2025-10-27 21:44:36 +0530

    fix

--- src/Executors/TypeCommandExecutor.h
+++ src/Executors/TypeCommandExecutor.h
@@ -7,14 +7,15 @@
 class TypeCommandExecutor : public ICommandExecutor {
 public:
     bool CanExecute(std::string& command) override {
-        std::string target = command.substr(5);
-        return
-         command.rfind("type ", 0) == 0 &&
-         BuiltinCommands::IsBuiltin(target);
+        return command.rfind("type ", 0) == 0;
     }
 
     void Execute(std::string& command) override {
         std::string target = command.substr(5);
-         std::cout << target << " is a shell builtin" << std::endl;
+        if (BuiltinCommands::IsBuiltin(target)) {
+            std::cout << target << " is a shell builtin" << std::endl;
+        } else {
+            std::cout << command << ": not found" << std::endl;
+        }
     }
 };
